1a1212153f4d081de1a8acfb83bb36b6a982a136
Armen Deroian <cosmic5173@gmail.com>
2023-06-22 21:40:20 -0400

    Update to 1.20

---
+++ Backend/src/main/java/moe/seikimo/altservice/event/Event.java
@@ -0,0 +1,9 @@
+package moe.seikimo.altservice.event;
+
+/**
+ * Represents an invokable and listenable event.
+ */
+public final class Event {
+
+
+}
--- Backend/src/main/java/moe/seikimo/altservice/utils/objects/absolute/NetworkConstants.java
+++ Backend/src/main/java/moe/seikimo/altservice/utils/objects/absolute/NetworkConstants.java
@@ -1,14 +1,14 @@
 package moe.seikimo.altservice.utils.objects.absolute;
 
 import org.cloudburstmc.protocol.bedrock.codec.BedrockCodec;
-import org.cloudburstmc.protocol.bedrock.codec.v582.Bedrock_v582;
+import org.cloudburstmc.protocol.bedrock.codec.v589.Bedrock_v589;
 
 /**
  * Constants related to networking.
  */
 public interface NetworkConstants {
     /* This will be the latest supported version. */
-    BedrockCodec PACKET_CODEC = Bedrock_v582.CODEC;
+    BedrockCodec PACKET_CODEC = Bedrock_v589.CODEC;
     /* This is the compression level used by Minecraft. */
     int COMPRESSION_LEVEL = 6;
 }
