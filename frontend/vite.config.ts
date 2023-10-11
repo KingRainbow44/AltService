bb67649aa14c2a8e4dfaeabd1256a6d3d135f726
KingRainbow44 <kobedo11@gmail.com>
2023-10-11 10:25:51 -0400

    (init) Add Tailwind & Formatting

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import preact from '@preact/preset-vite'
+import { defineConfig } from "vite";
+import preact from "@preact/preset-vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+import tailwind from "tailwindcss";
+import autoprefixer from "autoprefixer";
+
+import tailwindConfig from "./cfg/tailwind.config.js";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [preact()],
-})
+  plugins: [preact(), tsconfigPaths()],
+  css: {
+    postcss: {
+      plugins: [
+        tailwind(tailwindConfig),
+        autoprefixer()
+      ]
+    }
+  }
+});
