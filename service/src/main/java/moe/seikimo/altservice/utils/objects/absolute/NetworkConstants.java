ac6edc74656002cdcf300726c22936b8078672bc
KingRainbow44 <kobedo11@gmail.com>
2023-10-20 14:00:00 +0100

    (service) Use Minecraft `1.20.30`'s codec & palette

--- service/src/main/java/moe/seikimo/altservice/utils/objects/absolute/NetworkConstants.java
+++ service/src/main/java/moe/seikimo/altservice/utils/objects/absolute/NetworkConstants.java
@@ -1,14 +1,14 @@
 package moe.seikimo.altservice.utils.objects.absolute;
 
 import org.cloudburstmc.protocol.bedrock.codec.BedrockCodec;
-import org.cloudburstmc.protocol.bedrock.codec.v589.Bedrock_v589;
+import org.cloudburstmc.protocol.bedrock.codec.v618.Bedrock_v618;
 
 /**
  * Constants related to networking.
  */
 public interface NetworkConstants {
     /* This will be the latest supported version. */
-    BedrockCodec PACKET_CODEC = Bedrock_v589.CODEC;
+    BedrockCodec PACKET_CODEC = Bedrock_v618.CODEC;
     /* This is the compression level used by Minecraft. */
     int COMPRESSION_LEVEL = 6;
 }
